c00326eb9876835c88b7028f002501f730a6b612
vuyani <ngubovuyani@gmail.com>
2017-08-19 00:41:23 +0200

    Defined gameboard items' css styles.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Script connected!");
+const DOMGameBoard = document.querySelector("#game-board");
+
+function createSquare(classNames) {
+	const DOMSquare = document.createElement("div");
+	for (let className of classNames) {
+		DOMSquare.classList.add(className);
+	}
+	return DOMSquare;
+}
+
+const squares = [
+	["square", "wall"],
+	["square", "pill"],
+	["square", "power-pill"],
+	["square", "pacman"],
+	["square", "ghost", "blinky"],
+	["square", "ghost", "pinky"],
+	["square", "ghost", "inky"],
+	["square", "ghost", "clyde"],
+	["square", "ghost", "clyde", "scared"],
+];
+
+for (let squareClasses of squares) {
+	DOMGameBoard.appendChild(createSquare(squareClasses));
+}
