8df671d4a09ab2213b947089cbe74f8b4e3b2021
vuyani <ngubovuyani@gmail.com>
2017-08-19 01:40:04 +0200

    [Refactor] Extracted game's setup parameters into separate module.

--- app.js
+++ app.js
@@ -1,54 +1,10 @@
-const BOARD = [
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-	[1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
-	[1, 3, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 3, 1],
-	[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-	[1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
-	[1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
-	[1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
-	[0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0],
-	[0, 0, 0, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 0, 0, 0],
-	[1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1],
-	[1, 0, 0, 0, 2, 2, 2, 1, 0, 0, 0, 0, 1, 2, 2, 2, 0, 0, 0, 1],
-	[1, 1, 1, 1, 2, 1, 2, 1, 5, 6, 7, 8, 1, 2, 1, 2, 1, 1, 1, 1],
-	[0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0],
-	[0, 0, 0, 1, 2, 1, 2, 0, 0, 4, 0, 0, 0, 2, 1, 2, 1, 0, 0, 0],
-	[1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
-	[1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
-	[1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
-	[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-	[1, 3, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 3, 1],
-	[1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
-	[1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-];
-// units are in pixels
-const grid_size = BOARD[0].length;
-const cell_size = 20;
-const object_type = [
-	"blank",
-	"wall",
-	"pill",
-	"power-pill",
-	"pacman",
-	"blinky",
-	"pinky",
-	"inky",
-	"clyde",
-];
-// prettier-ignore
-const object_classes = {
-	"blank": [],
-	"wall": ["wall"],
-	"pill": ["pill"],
-	"power-pill": ["power-pill"],
-	"pacman": ["pacman"],
-	"blinky": ["ghost", "blinky"],
-	"pinky": ["ghost", "pinky"],
-	"inky": ["ghost", "inky"],
-	"clyde": ["ghost", "clyde"],
-};
+import {
+	LEVEL,
+	GRID_SIZE,
+	CELL_SIZE,
+	OBJECT_CLASSES,
+	OBJECT_TYPE,
+} from "./preload/setup";
 
 const DOMGrid = document.querySelector("#game-board");
 
@@ -64,10 +20,10 @@ function createSquare(classNames) {
 function createGrid(DOMGrid, board) {
 	board.forEach((row) => {
 		row.forEach((square) => {
-			DOMGrid.appendChild(createSquare(object_classes[object_type[square]]));
+			DOMGrid.appendChild(createSquare(OBJECT_CLASSES[OBJECT_TYPE[square]]));
 		});
 	});
 }
 
-DOMGrid.style.cssText = `grid-template-columns: repeat(${grid_size}, ${cell_size}px)`;
-createGrid(DOMGrid, BOARD);
+DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
+createGrid(DOMGrid, LEVEL);
