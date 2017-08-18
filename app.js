0716be7424b4e9b3428d8d7038bc1d9925099c00
vuyani <ngubovuyani@gmail.com>
2017-08-19 01:21:05 +0200

    Defined game's render logic.

--- app.js
+++ app.js
@@ -1,25 +1,73 @@
-const DOMGameBoard = document.querySelector("#game-board");
+const BOARD = [
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+	[1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
+	[1, 3, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 3, 1],
+	[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+	[1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
+	[1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+	[1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
+	[0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0],
+	[0, 0, 0, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 0, 0, 0],
+	[1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1],
+	[1, 0, 0, 0, 2, 2, 2, 1, 0, 0, 0, 0, 1, 2, 2, 2, 0, 0, 0, 1],
+	[1, 1, 1, 1, 2, 1, 2, 1, 5, 6, 7, 8, 1, 2, 1, 2, 1, 1, 1, 1],
+	[0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0],
+	[0, 0, 0, 1, 2, 1, 2, 0, 0, 4, 0, 0, 0, 2, 1, 2, 1, 0, 0, 0],
+	[1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
+	[1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+	[1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
+	[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+	[1, 3, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 3, 1],
+	[1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
+	[1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+// units are in pixels
+const grid_size = BOARD[0].length;
+const cell_size = 20;
+const object_type = [
+	"blank",
+	"wall",
+	"pill",
+	"power-pill",
+	"pacman",
+	"blinky",
+	"pinky",
+	"inky",
+	"clyde",
+];
+// prettier-ignore
+const object_classes = {
+	"blank": [],
+	"wall": ["wall"],
+	"pill": ["pill"],
+	"power-pill": ["power-pill"],
+	"pacman": ["pacman"],
+	"blinky": ["ghost", "blinky"],
+	"pinky": ["ghost", "pinky"],
+	"inky": ["ghost", "inky"],
+	"clyde": ["ghost", "clyde"],
+};
+
+const DOMGrid = document.querySelector("#game-board");
 
 function createSquare(classNames) {
 	const DOMSquare = document.createElement("div");
+	DOMSquare.classList.add("square");
 	for (let className of classNames) {
 		DOMSquare.classList.add(className);
 	}
 	return DOMSquare;
 }
 
-const squares = [
-	["square", "wall"],
-	["square", "pill"],
-	["square", "power-pill"],
-	["square", "pacman"],
-	["square", "ghost", "blinky"],
-	["square", "ghost", "pinky"],
-	["square", "ghost", "inky"],
-	["square", "ghost", "clyde"],
-	["square", "ghost", "clyde", "scared"],
-];
-
-for (let squareClasses of squares) {
-	DOMGameBoard.appendChild(createSquare(squareClasses));
+function createGrid(DOMGrid, board) {
+	board.forEach((row) => {
+		row.forEach((square) => {
+			DOMGrid.appendChild(createSquare(object_classes[object_type[square]]));
+		});
+	});
 }
+
+DOMGrid.style.cssText = `grid-template-columns: repeat(${grid_size}, ${cell_size}px)`;
+createGrid(DOMGrid, BOARD);
