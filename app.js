744614e07f3d3b26fd7b5b20945dc1cddff04f9b
vuyani <ngubovuyani@gmail.com>
2017-08-24 20:13:28 +0200

    [Refactor] Redefined game's start/play/pause logic.

--- app.js
+++ app.js
@@ -9,24 +9,30 @@ UI.render(DOMGrid, game.board);
 let refreshID;
 let isPaused = true;
 
-document.querySelector("#start-btn").addEventListener("click", () => {
-	if (!refreshID) {
-		isPaused = !isPaused;
-		game.start();
-		UI.render(DOMGrid, game.board);
-		refreshID = setInterval(refreshUI, 250);
-	}
-});
+document.querySelector("#start-btn").addEventListener("click", startGame);
 
 window.addEventListener("keydown", ({ key }) => {
 	if (key === " ") {
-		isPaused = !isPaused;
-		if (isPaused) clearInterval(refreshID);
-		else refreshID = setInterval(refreshUI, 250);
+		if (!refreshID) {
+			startGame();
+		} else {
+			isPaused = !isPaused;
+			if (isPaused) clearInterval(refreshID);
+			else refreshID = setInterval(refreshUI, 250);
+		}
 	}
 });
 
 function refreshUI() {
 	game.nextFrame();
 	UI.render(DOMGrid, game.board);
+}
+
+function startGame() {
+	if (!refreshID) {
+		isPaused = !isPaused;
+		game.start();
+		UI.render(DOMGrid, game.board);
+		refreshID = setInterval(refreshUI, 250);
+	}
 }
